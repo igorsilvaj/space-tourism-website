b589bd4efc004fb1c79850f5288dfc127e654da9
igorsilvaj <igorjsilvabiz@gmail.com>
2023-09-01 12:56:16 -0300

    :wrench:chore: update config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    formats: [
+      'image/avif', 'image/webp'
+    ],
+    remotePatterns: []
+  }
+}
 
 module.exports = nextConfig
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,88 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      sm: "0px", // => @media (min-width: 0px) { ... }
+      md: "768px", // => @media (min-width: 768px) { ... }
+      lg: "1440px", // => @media (min-width: 1024px) { ... }
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: '#FFFFFF',
+        secondary: '#D0D6F9',
+        tertiary: '#0B0D17',
       },
+      fontFamily: {
+        bellefair: ['var(--font-bellefair)'],
+        barlow: ['var(--font-barlow)'],
+        'barlow-condensed': ['var(--font-barlow-condensed)'],
+      },
+      fontSize: {
+        base: ['18px', {
+          lineHeight: '32px',
+        }],
+        buttonText: ['32px', {
+          letterSpacing: '2px',
+        }],
+        heading1: '150px',
+        heading2: '100px',
+        heading3: '56px',
+        heading4: '32px',
+        heading5: ['28px', {
+          letterSpacing: '4.75px',
+        }],
+        subheading1: '28px',
+        subheading2: ['14px', {
+          letterSpacing: '2.35px',
+        }],
+        navText: ['16px', {
+          letterSpacing: '2.7px',
+        }],
+
+
+        tabletTextBase: ['16px', {
+          lineHeight: '28px',
+        }],
+        tabletButtonText: ['32px', {
+          letterSpacing: '2px',
+        }],
+        tabletHeading1: '150px',
+        tabletHeading2: '80px',
+        tabletHeading3: '40px',
+        tabletHeading4: '24px',
+        tabletHeading5: ['20px', {
+          letterSpacing: '3.38px',
+        }],
+        tabletSubheading1: '28px',
+        tabletSubheading2: ['14px', {
+          letterSpacing: '2.36px',
+        }],
+        tabletNavText: ['16px', {
+          letterSpacing: '2.7px',
+        }],
+
+
+        mobileTextBase: ['15px', {
+          lineHeight: '25px',
+        }],
+        mobileButtonText: ['20px', {
+          letterSpacing: '1.25px',
+        }],
+        mobileHeading1: '80px',
+        mobileHeading2: '56px',
+        mobileHeading3: '24px',
+        mobileHeading4: '16px',
+        mobileHeading5: ['16px', {
+          letterSpacing: '2.7px',
+        }],
+        mobileSubheading1: '28px',
+        mobileSubheading2: ['14px', {
+          letterSpacing: '2.36px',
+        }],
+        mobileNavText: ['14px', {
+          letterSpacing: '2.36px',
+        }],
+      }
     },
   },
   plugins: [],
